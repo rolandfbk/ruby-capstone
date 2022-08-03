22cc80bb0c9669517bd185c9e84a88b109c18386
Michael Ugochukwu <mark.omulu@gmail.com>
2022-08-03 07:00:05 +0100

    Add game class

---
+++ base/game.rb
@@ -0,0 +1,19 @@
+require_relative './items'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?
+    current_year = Time.new.year
+    seniority = current_year - @last_played_at
+    super && seniority > 2
+  end
+end
