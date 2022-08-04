cf526c6a18709b69fb4de83fcc7ad8efe21b1078
Jake Felice <jasonfelice0@gmail.com>
2022-08-04 03:51:48 -0700

    Fix rubocop errors

--- data/data_album.rb
+++ data/data_album.rb
@@ -3,7 +3,7 @@ require_relative '../base/genre'
 require_relative '../base/music_album'
 require 'Date'
 
-def load_album_genre(my_albums, my_genres)
+def load_album_genre(myalbum, mygenre)
   if File.exist?('./data/albums.json')
     file = File.open('./data/albums.json')
 
@@ -13,23 +13,19 @@ def load_album_genre(my_albums, my_genres)
     else
       albums = JSON.parse(File.read('./data/albums.json'))
 
-      my_albums.each do |album|
-        name = album['name']
-        genre_name = album['genre']
-        spotify = album['spotify']
-        date = album['published']
-        genre = Genre.new(genre_name)
-        album = MusicAlbum.new(Date.parse(date), name, spotify)
-        my_albums << album
-        my_genres << genre
+      albums.each do |album|
+        genre = Genre.new(album['genre'])
+        album = MusicAlbum.new(Date.parse(album['published']), album['name'], album['spotify'])
+        myalbum << album
+        mygenre << genre
 
         genre.add_item(album)
       end
     end
     file.close
   else
-    my_albums << []
-    my_genres << []
+    myalbum << []
+    mygenre << []
   end
 end
 
